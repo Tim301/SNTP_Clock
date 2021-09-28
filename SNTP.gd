2117692e2cb8af06e824f60f50506352dcadf3a8
Tim301 <45065016+Tim301@users.noreply.github.com>
2021-09-29 00:06:44 +0200

    Add files via upload
    start implementation decoding ntp time

--- SNTP.gd
+++ SNTP.gd
@@ -1,5 +1,6 @@
 extends Node2D
-const NTP_HOST = "0.nettime.pool.ntp.org"
+#const NTP_HOST = "0.nettime.pool.ntp.org"
+const NTP_HOST = "193.190.198.10"
 const NTP_PORT = 123
 var o_peer_udp: PacketPeerUDP = PacketPeerUDP.new()
 
@@ -10,21 +11,13 @@ func synchronize():
 	
 	var send_packet = PoolByteArray()
 	send_packet.append_array([0x1B])
-	var s = OS.get_unix_time() + 2208988800	# Convert unix time (32 bit) to NTP (64 bit), add epoch offset.
-	print("unix: ", OS.get_unix_time())
-	print("sys: ",OS.get_system_time_secs())
-	print("sys ms : ",OS.get_system_time_msecs())
-	var ms = OS.get_system_time_msecs()
-	print((float(ms/1000.0) - int(ms/1000)))
-	var Ms = int(round((float(ms/1000.0) - int(ms/1000))*1000.0))
-	print("MS2: ", Ms)
-	var msstr = String(ms)
-	print(ms)
-	var us = int(msstr.substr(len(msstr)-3,len(msstr)-1) + "000")
-	us = (us<<32)/1000000
+	print("   send: ", OS.get_system_time_secs())
+	var ntp_s = OS.get_system_time_secs() + 2208988800	# Convert unix time to NTP time, add epoch offset.
+	var ntp_us = (int(OS.get_system_time_msecs() - stepify(OS.get_system_time_msecs(), 1000))<<32)/1000  # ntp_us = (unix_us<<32)/1000000
+	#print("ms: ",OS.get_system_time_msecs())
 	for n in range(39):
 		send_packet.append_array([0x00])
-	send_packet.append_array([(( s >> 24) & 0xff),(( s >> 16) & 0xff),(( s >> 8) & 0xff),(( s >> 0) & 0xff),(( us >> 24) & 0xff),(( us >> 16) & 0xff),(( us >> 8) & 0xff),(( us >> 0) & 0xff)])	
+	send_packet.append_array([(( ntp_s >> 24) & 0xff),(( ntp_s >> 16) & 0xff),(( ntp_s >> 8) & 0xff),(( ntp_s >> 0) & 0xff),(( ntp_us >> 24) & 0xff),(( ntp_us >> 16) & 0xff),(( ntp_us >> 8) & 0xff),(( ntp_us >> 0) & 0xff)])	
 	o_peer_udp.set_dest_address(NTP_HOST, NTP_PORT)
 	o_peer_udp.put_packet(send_packet)
 
@@ -40,4 +33,10 @@ func _process(delta):
 #					recv_packet[(4 * m) + 3]
 #					]
 #				)
-		print("leng: ", recv_packet.size())
+		var t4 = float(recv_packet[40] << 24) + (recv_packet[41] << 16) + (recv_packet[42] << 8) + (recv_packet[43] << 0) - 2208988800
+		var tu = (recv_packet[44] << 24) + (recv_packet[45] << 16) + (recv_packet[46] << 8) + (recv_packet[47] << 0)
+		var tu2 = float(((recv_packet[44] << 24) + (recv_packet[45] << 16) + (recv_packet[46] << 8) + (recv_packet[47] << 0))*1000 >> 32)/1000
+		print(float(tu*1000>>32)/1000)
+		print(t4+(float(tu*1000>>32)/1000))
+		print("Receive: ", t4)
+		
